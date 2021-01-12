b28ee0ac923e4591572f6bbeda83ab18cea637e1
Shawn <banasick@mail.kobe-c.ac.jp>
2021-01-12 22:31:46 +0900

    added question object for samples

--- src/Sections/Config/Config.jsx
+++ src/Sections/Config/Config.jsx
@@ -12,6 +12,46 @@ import UserDropdown from "../../Utils/UserDropdown";
 
 const TextImage = React.lazy(() => import("./textQuestion"));
 
+const surveyQuestionObject = {
+  text: {
+    label: "Age",
+    note: "Please enter your year of birth (YYYY, eg. 1980).",
+    required: "true",
+    maxlength: 4,
+    restricted: "0-9",
+  },
+  textArea: {
+    label: "Comments",
+    required: "false",
+  },
+  radio: {
+    label: "Gender",
+    note: "Please select your gender",
+    required: "true",
+    options: "female;male",
+  },
+  rating2: {
+    label: "Please answer the following questions.",
+    required: "false",
+    scale: "Yes;No;",
+    options: "There's a car in my household;I own a car for myself",
+  },
+  checkbox: {
+    label: "What kind of transportation do you prefer?",
+    required: "false",
+    options: "Car;Railroad;Bike;",
+  },
+  select: {
+    label: "What kind of transportation do you prefer?",
+    required: "false",
+    options: "Car;Railroad;Bike",
+  },
+  note: {
+    bg: "false",
+    option: "All fields marked with an * are mandatory",
+  },
+};
+
 const handleClick = () => {
   const data = `
     <config version="1.0" htmlParse="false">
