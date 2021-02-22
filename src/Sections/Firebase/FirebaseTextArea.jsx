dc2aa4dc8a1ebcc3e0bbb6f3541c2ff964839dfe
Shawn <banasick@mail.kobe-c.ac.jp>
2021-02-23 06:21:14 +0900

    fixed firebase textarea parse error

--- src/Sections/Firebase/FirebaseTextArea.jsx
+++ src/Sections/Firebase/FirebaseTextArea.jsx
@@ -5,40 +5,44 @@ import appState from "../../GlobalState/appState";
 
 const handleChange = (event) => {
   let infoInput = event.target.value;
-
-  if (infoInput === undefined || infoInput === null || infoInput.length === 0) {
-    return;
-  }
-
   appState.currentFirebaseInfo = infoInput;
 
-  let arr = infoInput.split(/\r\n|\r|\n/g);
-  let filteredArray = arr.filter(function (el) {
-    return el;
-  });
-  const additionalInfo = `  var rootRef = firebase.database().ref();`;
-  const endLine = filteredArray.pop();
-  filteredArray.push(additionalInfo, endLine);
-  let filteredArrayText = ``;
-  let shouldConcat = false;
-  for (let i = 0; i < filteredArray.length; i += 1) {
-    let iterator = filteredArray[i];
-    let substring1 = iterator.substring(0, 12);
-    substring1 = substring1.replace(/\s/g, "");
-    let substring2 = substring1.substring(0, 2);
-    substring1 = substring1.substring(0, 8);
-    if (substring1 === "<script>") {
-      shouldConcat = true;
+  // if (infoInput === undefined) {
+  //   return;
+  // }
+
+  if (infoInput.length !== 0 && infoInput !== null && infoInput !== undefined) {
+    let arr = infoInput.split(/\r\n|\r|\n/g);
+    let filteredArray = arr.filter(function (el) {
+      return el;
+    });
+
+    // setup array values
+    const additionalInfo = `  var rootRef = firebase.database().ref();`;
+    const endLine = filteredArray.pop();
+    filteredArray.push(additionalInfo, endLine);
+    let filteredArrayText = ``;
+    let shouldConcat = false;
+
+    // cycle through text
+    for (let i = 0; i < filteredArray.length; i += 1) {
+      let iterator = filteredArray[i];
+      let substring1 = iterator.substring(0, 12);
+      substring1 = substring1.replace(/\s/g, "");
+      let substring2 = substring1.substring(0, 2);
+      substring1 = substring1.substring(0, 8);
+      if (substring1 === "<script>") {
+        shouldConcat = true;
+      }
+      if (shouldConcat && substring2 !== "//") {
+        filteredArrayText = filteredArrayText + iterator;
+      }
+      if (substring1 === "</script") {
+        shouldConcat = false;
+      }
     }
-    if (shouldConcat && substring2 !== "//") {
-      filteredArrayText = filteredArrayText + iterator;
-    }
-    if (substring1 === "</script") {
-      shouldConcat = false;
-    }
+    appState.firebaseInfo = filteredArrayText;
   }
-  // console.log(filteredArrayText);
-  appState.firebaseInfo = filteredArrayText;
 };
 
 const FirebaseTextArea = () => {
